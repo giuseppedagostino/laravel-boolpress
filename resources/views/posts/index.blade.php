5cc66bba86dfb4d6954217a9860537f36612e9f7
Giuseppe <giuseppe.dagostino.d@gmail.com>
2021-02-18 18:08:25 +0100

    Collegamento CSS
    1 - Creo e collego il database
    2 - Creo il layout main e i partials
    3 - Collego il css e creo tutti i partials scss

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang="it">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Laravel-boolpress</title>
-</head>
+@extends('layouts.main')
 
-<body>
-  <h1>Pagina index di Post</h1>
-</body>
-</html>
+@section('content')
+    <h1>Pagina Index di Posts</h1>
+@endsection
