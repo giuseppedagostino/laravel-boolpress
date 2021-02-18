a43cc29de02616338ac4dc86ff5e0d34d70a904a
Giuseppe <giuseppe.dagostino.d@gmail.com>
2021-02-18 17:49:53 +0100

    Creazione controller
    1 - Creazione del PostController
    2 - Creazione della cartella posts e delle rotte associate
    3 - Creazione index.blade

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// rotte gestite dal PostController
+Route::resource('posts', 'PostController');
